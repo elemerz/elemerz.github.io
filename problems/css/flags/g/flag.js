4efda6addcd0b6130639757181e73b1fa214c9eb
elemerz <elemerz@gmail.com>
2018-11-14 11:02:43 +0200

    Added Japan flag with Bull's eye

--- problems/css/flags/g/flag.js
+++ problems/css/flags/g/flag.js
@@ -1,14 +1,16 @@
 window.onload = function () {
   var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg"),
     svgNS = svg.namespaceURI,
-    appendRect = function(svg, x, y, w, h, color){
+    appendRect = function(svgParam, x, y, w, h, color){
       var rect = document.createElementNS(svgNS,'rect');
+
       rect.setAttribute('x',x);
       rect.setAttribute('y',y);
       rect.setAttribute('width',w);
       rect.setAttribute('height',h);
       rect.setAttribute('fill',color);
-      svg.appendChild(rect);
+
+      svgParam.appendChild(rect);
     };
 
   appendRect(svg,0,0,100,100,'red');
