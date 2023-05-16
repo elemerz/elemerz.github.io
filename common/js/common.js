e8c013b50cbc011bf0ed2a7ffe3efb0132919b4c
elemerz <elemer.zagoni@endava.com>
2023-05-16 19:42:21 +0200

    Added Angular resources

--- common/js/common.js
+++ common/js/common.js
@@ -4,7 +4,8 @@
     $(function(){
         $(document)
             .on("click", "ul.main li>span", WT.expandCollapseNode)
-            .on("click", "ul.main > .expand-all", WT.expandCollapseAll);
+            .on("click", "ul.main > .expand-all", WT.expandCollapseAll)
+            .on("click", "a[href^=\"#local-\"]", WT.displayLocalCodeSnippet);
     });
 
     var WT={
@@ -14,6 +15,24 @@
         expandCollapseAll: function(){
             $(this).toggleClass("expanded");
             $("li > ul")[$(this).is(".expanded") ? "show" : "hide"]();
+        },
+        displayLocalCodeSnippet: function(evt){
+            console.log(evt.currentTarget.href);
+            var hashIdx = evt.currentTarget.href.lastIndexOf("#");
+            if (hashIdx !== -1) {
+                var targetId = evt.currentTarget.href.substr(hashIdx + 1);
+                var $target = $("#" + targetId);
+                if ($target.length) {
+                    const isHidden = $target.is(":hidden");
+                    $('pre[id^="local-"]').hide();
+                    if (isHidden) {
+                        $target.show();
+                    } else {
+                        $target.hide();
+                    }
+                }
+            }
+            return false
         }
     };
 }(jQuery))
