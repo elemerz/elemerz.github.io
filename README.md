61f9fee2a6722be45ea294eaa59395ec967d8aad
Elemer Zagoni <elemerz@gmail.com>
2023-12-09 10:48:48 +0200

    TreeView: Phase 1: Expander already animates. Children do not animate yet.

--- README.md
+++ README.md
@@ -12,6 +12,7 @@ Eli's Github pages:
   - ![BackgroundColorizer](my-stuff/background-colorizer/favicon-16x16.png)[BackgroundColorizer](my-stuff/background-colorizer/index.html)
   - ![Animated Gradient Border](my-stuff/animated-border/favicon-16x16.png)[Animated Gradient Border](my-stuff/animated-border/index.html)
   - ![Slide Up/Down](my-stuff/slide-up-down/favicon-16x16.png)[Slide Up/Down](my-stuff/slide-up-down/index.html)
+  - ![TreeView](my-stuff/tree-view/favicon-16x16.png)[TreeView](my-stuff/tree-view/index.html)
   - ![RoundTable](my-stuff/round-table/favicon-16x16.png)[RoundTable](my-stuff/round-table/index.html)
   - ![ImageAsTable](my-stuff/img-as-table/favicon-16x16.png)[ImageAsTable](my-stuff/img-as-table/index.html)
   - ![GithubCopilot](my-stuff/copilot/favicon-16x16.png)[GithubCopilot](my-stuff/copilot/index.html)
---
+++ my-stuff/tree-view/tree-view.js
@@ -0,0 +1,91 @@
+const treeModel={
+    label:'TreeRoot', icon:'root.png', expanded: true,
+    children: [
+        {label:'Fruits', icon:'fruits.png', expanded: true,
+            children:[
+                {label:'Apple', icon:'apple.png', expanded: true,
+                    children: [
+                        {label:'Granny Smith', icon:'granny-smith.png', expanded: false, children:[]},
+                        {label:'Red Delicious', icon:'red-delicious.png', expanded: false, children:[]},
+                        {label:'Golden', icon:'golden.png', expanded: false, children:[]},
+                        {label:'Pink Lady', icon:'pink-lady.png', expanded: false, children:[]}
+                    ]
+                },
+                {label:'Pear', icon:'pear.png', expanded: true,
+                    children: [
+                        {label:'Williams', icon:'williams.png', expanded: false, children:[]},
+                        {label:'Abate', icon:'abate.png', expanded: false, children:[]},
+                        {label:'Concorde', icon:'concorde.png', expanded: false, children:[]},
+                        {label:'Anjou', icon:'anjou.png', expanded: false, children:[]}
+                    ]
+                },
+                {label:'Plum', icon:'plum.png', expanded: true,
+                    children: [
+                        {label:'Besztercei', icon:'besztercei.png', expanded: false, children:[]},
+                        {label:'Fosóka', icon:'fosoka.png', expanded: false, children:[]},
+                        {label:'Ruby Queen', icon:'ruby-queen.png', expanded: false, children:[]},
+                        {label:'Damson', icon:'damson.png', expanded: false, children:[]}
+                    ]
+                }
+            ]
+        },
+        {label:'Vegetables', icon:'vegetables.png', expanded: true,
+            children:[
+                {label:'Potato', icon:'potato.png', expanded: true,
+                    children: [
+                        {label:'Yukon Gold', icon:'yukon-gold.png', expanded: false, children:[]},
+                        {label:'Purple Majesty', icon:'purple-majesty.png', expanded: false, children:[]},
+                        {label:'King Edward', icon:'king-edward.png', expanded: false, children:[]},
+                        {label:'Desiree', icon:'desiree', expanded: false, children:[]}
+                    ]
+                },
+                {label:'Tomato', icon:'tomato.png', expanded: true,
+                    children: [
+                        {label:'Roma ', icon:'roma.png', expanded: false, children:[]},
+                        {label:'Cherry', icon:'cherry.png', expanded: false, children:[]},
+                        {label:'Sungold', icon:'sungold.png', expanded: false, children:[]},
+                        {label:'Indigo Rose', icon:'indigo-rose.png', expanded: false, children:[]}
+                    ]
+                },
+                {label:'Carrot', icon:'carrot.png', expanded: true,
+                    children: [
+                        {label:'Danvers', icon:'danvers.png', expanded: false, children:[]},
+                        {label:'Imperator', icon:'imperator.png', expanded: false, children:[]},
+                        {label:'Nelson', icon:'nelson.png', expanded: false, children:[]},
+                        {label:'Flyaway', icon:'flyaway.png', expanded: false, children:[]}
+                    ]
+                }
+            ]
+        }
+    ]
+};
+
+function createTree(node, parentElement) {
+    const treeNode = document.createElement('div');
+    treeNode.className = 'node';
+    treeNode.innerHTML = `<span class="expander"></span><span class="node-label">${node.label}</span>`;
+    parentElement.appendChild(treeNode);
+
+    if (node.children && node.children.length > 0) {
+        const childrenInner = document.createElement('div');
+        childrenInner.className = 'children-inner';
+        const childrenContainer = document.createElement('div');
+        childrenContainer.className = 'children';
+        childrenContainer.appendChild(childrenInner);
+        treeNode.appendChild(childrenContainer);
+
+
+        node.children.forEach(child => createTree(child, childrenInner));
+        treeNode.classList.add(node.expanded ? 'expanded' : 'collapsed');
+        treeNode.querySelector('.expander').addEventListener('click', function() {
+            treeNode.classList.toggle('expanded');
+            treeNode.classList.toggle('collapsed');
+        });
+    }
+}
+
+function initTree() {
+    const rootElement = document.getElementById('treeView');
+    rootElement.innerHTML = ''; //make the method idempotent
+    createTree(treeModel, rootElement);
+}
